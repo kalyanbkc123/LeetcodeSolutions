6f8e35ca1de59325b9fbaaf9e89e3a5795639f26
Kalyan-Charkavarthi <123096261+kalyanbkc123@users.noreply.github.com>
2024-08-13 15:56:15 -0700

    Test

---
+++ AddTwoNumbers.java
@@ -0,0 +1,7 @@
+public class AddTwoNumbers {
+
+    public static int addTwo(int a,int b)
+    {
+        return a+b;
+    }
+}
---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int c = AddTwoNumbers.addTwo(3,5);
+        System.out.println(c);
+    }
+}
+

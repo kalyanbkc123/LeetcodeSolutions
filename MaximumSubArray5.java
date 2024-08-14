003086ee2f6eb32aa30c2924cea827268d8274d7
Kalyan-Charkavarthi <123096261+kalyanbkc123@users.noreply.github.com>
2024-08-14 15:18:30 -0700

    contains duplicate 04

--- MaximumSubArray5.java
+++ MaximumSubArray5.java
@@ -27,4 +27,46 @@ public class MaximumSubArray5 {
 
         return max;
     }
+
+    // Better approach
+    // Time Complexity :: O(N^2)
+    // Space Complexity :: O(1)
+
+    public static int maxSubArray2(int[] nums)
+    {
+        int maxi = Integer.MIN_VALUE;
+        int n = nums.length;
+        for(int i=0; i < n; i++)
+        {
+            int sum = 0;
+            for(int j = i; j <= i; j++)
+            {
+                sum += nums[i];
+
+                maxi = Math.max(maxi, sum);
+            }
+        }
+        return maxi;
+    }
+
+    // Optimal Approach
+    public static int maxSubArray3(int[] nums)
+    {
+        int n = nums.length;
+        int sum = 0;
+        int maxi = Integer.MIN_VALUE;
+        for(int i=0; i <n; i++)
+        {
+           sum += nums[i];
+            if(sum > maxi)
+            {
+                maxi = sum;
+            }
+            if(sum <  0)
+            {
+                sum = 0;
+            }
+        }
+        return maxi;
+    }
 }
